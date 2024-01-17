e48e95c8ceb88647aa75b1c83eefc5d5c032f9eb
Areeba Rafiq <140241495+Areeba000@users.noreply.github.com>
2024-01-17 11:53:44 +0500

    Update contract.sol

--- etherwallet/contract.sol
+++ etherwallet/contract.sol
@@ -1,44 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MessageStorage {
-    address payable public  owner;
-    string public message;
-    uint256 public updateCount;
-    enum msgplacecondition{available,rented}
-    msgplacecondition newmessage;
+contract etherwallet{
+address public owner;
+string public message;
+address public placeholder;
+enum conditioncheck{available,notavailable}
+conditioncheck newmsg;
+constructor(string memory _initialmessage) {
+    owner=payable (msg.sender);
+    message=_initialmessage;
+    newmsg=conditioncheck.available;
 
-    constructor(string memory initialMessage) {
-        owner =payable(msg.sender);
-        message = initialMessage;
-        updateCount = 0;
-        newmessage=msgplacecondition.available;
-    }
+}
+modifier placeholderpersons(){
+     require(placeholder == msg.sender ||owner== msg.sender, "you are not authorized to return place");
+     _;
+}
+function changemassage(string memory _newmessage)external payable {
+   require(msg.value==1 ether,"not enough ether");
+   message=_newmessage;
+   placeholder=msg.sender;
+   newmsg=conditioncheck.notavailable;
 
-    function updateMessage(string memory newMessage) public payable  {
-         require(newmessage == msgplacecondition.available,"the place is already rented");
-        if(msg.value==1 ether){
-             message = newMessage;
-        }else{
-            payable(msg.sender).transfer(msg.value);
-        }
-        updateCount ++;
-        newmessage=msgplacecondition.rented;
+}
+function returnplace()external placeholderpersons{
+        newmsg = conditioncheck.available;
+}
+function getcondition()external view returns(conditioncheck){
+     return newmsg;
 
-    }
-
-    function getcondition()public view returns(msgplacecondition){
-        return newmessage;
-
-    }
-    
-    function returnplace()public{
-        newmessage = msgplacecondition.available;
-
-    }
-    function withdraw()public payable  {
-        require(owner==msg.sender,"you are not owner");
-        owner.transfer(address(this).balance);
-   }
-
+}
+function withdraw()external {
+    require(owner==msg.sender,"only owner can with draw money");
+  payable(owner).transfer(address(this).balance);
+}
 }
